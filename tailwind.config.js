5cfdbbe1c09298902261e6275c2bddb1a350af24
nikan-af <>
2021-07-23 19:14:52 -0400

    Implemented the ace editor for HTML and JS.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const purge = process.env.NODE_ENV === "production";
+
+module.exports = {
+  purge: { enabled: purge, content: ["./build/**/*.html"] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
